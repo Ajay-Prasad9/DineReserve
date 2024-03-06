92afc0030533bc1ad9360a4c62d4f696ca9f943c
Ajay-Prasad9 <ajay.ap0986@gmail.com>
2024-03-06 12:21:51 +0530

    DineReserve

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Yummm</div>
+          <div className="right">
+            <p>Greater Noida, India</p>
+            <p>Open: 10:00 AM - 09:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Ajay Prasad</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By AJ.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
